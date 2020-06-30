367081aa0d277516b4273649f7ebfaefc6d099e9
Marlen <marlen.arbelo.external@atos.net>
2020-06-30 15:35:46 +0100

    Faltaban componentes, app.module mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+import {FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
@@ -12,7 +17,47 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SupplierComponent } from './supplier/supplier.component';
 import { SuformComponent } from './supplier/suform.component';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { OformComponent } from './order/oform.component';
 
+registerLocaleData(localeEs, 'es');
+
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {path: 'home', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'shop/form', component: SformComponent},
+  {path: 'shop/form/:idShop', component: SformComponent},
+  {path: 'client', component: ClientComponent},
+  {path: 'client/form', component: CformComponent},
+  {path: 'client/form/:idClient', component: CformComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'product/form', component: PformComponent},
+  {path: 'product/form/:idProduct', component: PformComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'order/form', component: OformComponent},
+  {path: 'order/form/:idOrder', component: OformComponent},
+  {path: 'supplier', component: SupplierComponent},
+  {path: 'supplier/form', component: SuformComponent},
+  {path: 'supplier/form/:idSupplier', component: SuformComponent},
+
+  { path: "clients/page/:page", component: ClientComponent},
+  { path: "clients/page/:page/:clientName", component: ClientComponent},
+
+  { path: "shops/page/:page", component: ShopComponent},
+  { path: "shops/page/:page/:shopName", component: ShopComponent},
+
+  { path: "products/page/:page", component: ProductComponent},
+  { path: "products/page/:page/:productName", component: ProductComponent},
+
+  { path: "orders/page/:page", component: OrderComponent},
+  { path: "orders/page/:page/:orderName", component: OrderComponent},
+
+  { path: "suppliers/page/:page", component: SupplierComponent},
+  { path: "suppliers/page/:page/:name", component: SupplierComponent},
+
+  ]
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +70,10 @@ import { SuformComponent } from './supplier/suform.component';
     HeaderComponent,
     FooterComponent,
     SupplierComponent,
-    SuformComponent
+    SuformComponent,
+    HomeComponent,
+    OrderComponent,
+    OformComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/order/oform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oform',
+  templateUrl: './oform.component.html',
+  styleUrls: ['./oform.component.css']
+})
+export class OformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
